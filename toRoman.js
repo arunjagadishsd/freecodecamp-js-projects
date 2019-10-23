c610446779b6b0dca2c2e45a9edd92a0e06ed208
AJ <arunjagadishsd@gmail.com>
2019-10-23 16:34:31 +0530

    Completed the  toRomannumerals

--- toRoman.js
+++ toRoman.js
@@ -1,19 +1,19 @@
 function convertToRoman(num) {
     const romanNumerals =
     {
-        1000: 'M', 900: 'CM', 500: 'D', 400: 'DM', 100: 'C', 90: 'CX',
-        50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 1: 'I'
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'DM', 100: 'C', 90: 'XC',
+        50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
     }
-    let numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 1];
+    const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     let converted = '';
     for (let i of numbers) {
-        let div = Math.floor(num / i);
+        let quotient = Math.floor(num / i);
         num = num % i
-        if (div >= 1) {
-            converted += romanNumerals[i].repeat(div);
+        if (quotient >= 1) {
+            converted += romanNumerals[i].repeat(quotient);
         }
     }
     return converted;
 }
 
-convertToRoman(49);
+convertToRoman(39)
