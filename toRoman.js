f914b5f6eac4df3c8ec228953b08db36d7afc0b7
AJ <arunjagadishsd@gmail.com>
2019-10-23 12:38:59 +0530

    Converted to roman numerals

--- toRoman.js
+++ toRoman.js
@@ -1,7 +1,18 @@
 function convertToRoman(num) {
-    const romanNumerals = {1:'I',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M'}
-    
-    return num;
+    const romanNumerals = 
+        { 1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V',1:'I'}
+    let numbers = [1000,500,100,50,10,5,1];
+    let converted = '';
+    for (let i of numbers){
+        console.log(i);
+        let div = Math.floor(num/i);
+        num = num % i
+        if (div>=1){
+            converted += romanNumerals[i].repeat(div);
+        }
+    }
+    console.log(converted);
+    return converted;
 }
 
 convertToRoman(36);
