d22a9c21f60d4a5975dcd99d1e6a89c6cfb4cbe1
AJ <arunjagadishsd@gmail.com>
2019-10-23 17:04:10 +0530

    Added the comments

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,9 +1,13 @@
 function rot13(str) { // LBH QVQ VG!
     let cipheredText='';
+    // Looping through the each char at string
     for (let char of str) {
+        // Checking if the character is uppercase letter
         if (char.charCodeAt() <= 90 && char.charCodeAt() >= 65) {
             let uCode = char.charCodeAt() - 13;
+            // To check if the letter is below the unicode of the uppercase letters
             if (uCode < 65) {
+                // subtracting the difference between 65 and the unicode
                 uCode = 90 - (64 - uCode);
             }
             let newChar = String.fromCharCode(uCode);
