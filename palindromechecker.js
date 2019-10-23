984047a237c0b5e3cdac9f4873d7540cbb589a63
AJ <arunjagadishsd@gmail.com>
2019-10-23 12:08:12 +0530

    Initail Commit

---
+++ palindromechecker.js
@@ -0,0 +1,11 @@
+function palindrome(str) {
+    // Sanitize the string
+    let isPalindrome = false;
+    str = str.replace(/[^0-9A-Z]+/gi, "").toLowerCase();
+    // Check if plaindrome
+    return true;
+}
+
+
+
+palindrome("eye");
