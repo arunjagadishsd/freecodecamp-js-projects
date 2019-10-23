36093097af39cd438714acdb2434773b867f2010
AJ <arunjagadishsd@gmail.com>
2019-10-23 16:35:31 +0530

    fixed a bug

--- toRoman.js
+++ toRoman.js
@@ -1,7 +1,7 @@
 function convertToRoman(num) {
     const romanNumerals =
     {
-        1000: 'M', 900: 'CM', 500: 'D', 400: 'DM', 100: 'C', 90: 'XC',
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC',
         50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
     }
     const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
