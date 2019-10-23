9e4af2f52226c9844f4ba104a60ec3deffd3f7c3
AJ <arunjagadishsd@gmail.com>
2019-10-23 17:00:39 +0530

    finished Ciphered text

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,11 +1,21 @@
 function rot13(str) { // LBH QVQ VG!
+    let cipheredText='';
     for (let char of str) {
-        let uCode = char.charCodeAt() - 13;
-        let a = String.fromCharCode(uCode - 13);
-        console.log(a);
+        if (char.charCodeAt() <= 90 && char.charCodeAt() >= 65) {
+            let uCode = char.charCodeAt() - 13;
+            if (uCode < 65) {
+                uCode = 90 - (64 - uCode);
+            }
+            let newChar = String.fromCharCode(uCode);
+            cipheredText += newChar;
+        }
+        else{
+            cipheredText += char;
+        }
     }
-    return str;
+    return cipheredText;
 }
 
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC"));
+ 
