4f54011d5fcb0941919bf34e2ff847e70ad58705
AJ <arunjagadish@aspigrow.com>
2019-10-26 14:38:47 +0530

    To implement length and () check in regex

--- telephonecheck.js
+++ telephonecheck.js
@@ -1,7 +1,11 @@
 function telephoneCheck(str) {
-    let regex = /[(0-9)0-9]+$/;
+    let numbers = str.match(/[0-9]/g).join("");
+    let valid1 = numbers.length == + 10 || (numbers.length == 11 && numbers.startsWith('1'));
+    console.log(numbers);
+    let regex = /[(0-9){3}0-9]+/;
 
-    return (regex.test(str));
+    return (regex.test(str) && valid1);
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("-1 (757) 622-7382"));
+ 
