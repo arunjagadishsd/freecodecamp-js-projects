9da9ec4527185c878a6d6236c2bcd718f008fa45
aj <arunjagadishsd@gmail.com>
2020-07-05 10:07:08 +0530

    Started telephone checker

--- telephonecheck.js
+++ telephonecheck.js
@@ -1,11 +1,18 @@
 function telephoneCheck(str) {
+    let valid = /[0-9(]/.test(str[0])
+    let valid2 = true;
+    if (str.includes('(') || str.includes(')')) {
+        valid2 = (str.includes('(') && str.includes(')'));
+    }
     let numbers = str.match(/[0-9]/g).join("");
     let valid1 = numbers.length == + 10 || (numbers.length == 11 && numbers.startsWith('1'));
     console.log(numbers);
-    let regex = /[(0-9){3}0-9]+/;
+    let regex = /[(\d{3}\)0-9]+/;
 
-    return (regex.test(str) && valid1);
+    let bwBracket = str.match(/\((.*)\)/).join("");
+    console.log('bwBracket', bwBracket)
+
+    return (regex.test(str) && valid1 && valid && valid2);
 }
 
-console.log(telephoneCheck("-1 (757) 622-7382"));
- 
+console.log(telephoneCheck("1 (555) 555-5555"));
